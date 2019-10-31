d375dfdfd9772e4f060af84043f08e38dfddab23
lihaosu <lihaosu429@gmail.com>
2019-10-31 18:51:54 +1100

    Add a collision detection script for the walls when they are hit by a wall

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float explosionSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, explosionSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
