a92253c10025087786a76f94057c8c2917bb7d27
lihaosu <lihaosu429@gmail.com>
2019-10-31 19:26:20 +1100

    Implement a simple tutorial

--- Assets/Scripts/Catapult.cs
+++ Assets/Scripts/Catapult.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Catapult : MonoBehaviour
 {
+    enum TutorialStage {RotateLeft,RotateRight,ForceIncrease,ForceDecrease,Fire,Reload,Goal,Done};
+
+    public GameObject tutorialText;
     public GameObject myBall;
     private GameObject curBall = null;
     private float ballForce = 0f;
     private float originalForce = 0f;
+    private TutorialStage tutorialStage = TutorialStage.RotateLeft;
 
     // Start is called before the first frame update
     void Start()
     {
+        UpdateTutorialText();
         setBall(Instantiate(myBall, transform.Find("Spawn").position, Quaternion.identity));
+    }
+
+    private string CalcTutorialText()
+    {
+        switch (tutorialStage)
+        {
+            case TutorialStage.RotateLeft:
+                return "Press D to rotate the catapult to the left";
+            case TutorialStage.RotateRight:
+                return "Press A to rotate the catapult to the right";
+            case TutorialStage.ForceIncrease:
+                return "Press W to increase the force of the catapult";
+            case TutorialStage.ForceDecrease:
+                return "Press S to decrease the force of the catapult";
+            case TutorialStage.Fire:
+                return "Press Space to fire";
+            case TutorialStage.Reload:
+                return "Press R to reload the catapult";
+            case TutorialStage.Goal:
+                return "Destroy the enemy towers!";
+            case TutorialStage.Done:
+                return "";
+        }
+        return "";
+    }
+    private void UpdateTutorialText()
+    {
+        tutorialText.GetComponent<Text>().text = CalcTutorialText();
+    }
+    private IEnumerator AdvanceTutorialStageCoroutine()
+    {
+        if (tutorialStage < TutorialStage.Done)
+        {
+            tutorialStage = tutorialStage + 1;
+        }
+        UpdateTutorialText();
+        if (tutorialStage == TutorialStage.Goal)
+        {
+            yield return new WaitForSeconds(5);
+            AdvanceTutorialStage();
+        }
+    }
+    private void AdvanceTutorialStage()
+    {
+        StartCoroutine(AdvanceTutorialStageCoroutine());
     }
 
     // Update is called once per frame
@@ -21,30 +72,36 @@ public class Catapult : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(Launch());
+            if (tutorialStage == TutorialStage.Fire) AdvanceTutorialStage();
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             StartCoroutine(Reel());
+            if (tutorialStage == TutorialStage.Reload) AdvanceTutorialStage();
         }
         if (Input.GetKey(KeyCode.D)) {
             if (transform.parent.rotation.y > -0.60f) {
                 transform.parent.Rotate(0, -1f, 0);
             }
+            if (tutorialStage == TutorialStage.RotateLeft) AdvanceTutorialStage();
         }
         if (Input.GetKey(KeyCode.A)) {
             if (transform.parent.rotation.y < 0.60f) {
                 transform.parent.Rotate(0, 1f, 0);
             }
+            if (tutorialStage == TutorialStage.RotateRight) AdvanceTutorialStage();
         }
         if (Input.GetKey(KeyCode.W)) {
             if (ballForce < (originalForce * 2)) {
                 ballForce += (originalForce / 100f);
             }
+            if (tutorialStage == TutorialStage.ForceIncrease) AdvanceTutorialStage();
         }
         if (Input.GetKey(KeyCode.S)) {
             if (ballForce > (originalForce / 20)) {
                 ballForce -= (originalForce / 100f);
             }
+            if (tutorialStage == TutorialStage.ForceDecrease) AdvanceTutorialStage();
         }
     }
 
