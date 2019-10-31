99d9117cfad454a8c74eae1cf122cd83099a90bf
lihaosu <lihaosu429@gmail.com>
2019-10-31 20:07:27 +1100

    Implement a simple tutorial, collision detection, prevention of movement while reloading and prevention of reloading multiple times

--- Assets/Scripts/Catapult.cs
+++ Assets/Scripts/Catapult.cs
@@ -5,16 +5,67 @@ using UnityEngine.UI;
 
 public class Catapult : MonoBehaviour
 {
+    enum TutorialStage {RotateLeft,RotateRight,ForceIncrease,ForceDecrease,Fire,Reload,Goal,Done};
+
+    public GameObject tutorialText;
     public GameObject myBall;
     private GameObject curBall = null;
     private float ballForce = 0f;
     private float originalForce = 0f;
     public Slider slider;
+    private TutorialStage tutorialStage = TutorialStage.RotateLeft;
+    private bool isReloading = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        UpdateTutorialText();
         setBall(Instantiate(myBall, transform.Find("Spawn").position, Quaternion.identity));
+    }
+
+    private string CalcTutorialText()
+    {
+        switch (tutorialStage)
+        {
+            case TutorialStage.RotateLeft:
+                return "Press D to rotate the catapult to the left";
+            case TutorialStage.RotateRight:
+                return "Press A to rotate the catapult to the right";
+            case TutorialStage.ForceIncrease:
+                return "Press W to increase the force of the catapult";
+            case TutorialStage.ForceDecrease:
+                return "Press S to decrease the force of the catapult";
+            case TutorialStage.Fire:
+                return "Press Space to fire";
+            case TutorialStage.Reload:
+                return "Press R to reload the catapult, you cannot move while reloading";
+            case TutorialStage.Goal:
+                return "Destroy the enemy towers!";
+            case TutorialStage.Done:
+                return "";
+        }
+        return "";
+    }
+    private void UpdateTutorialText()
+    {
+        tutorialText.GetComponent<Text>().text = CalcTutorialText();
+    }
+    private IEnumerator AdvanceTutorialStageCoroutine()
+    {
+        if (tutorialStage < TutorialStage.Done)
+        {
+            tutorialStage = tutorialStage + 1;
+        }
+        UpdateTutorialText();
+        if (tutorialStage == TutorialStage.Goal)
+        {
+            yield return new WaitForSeconds(5);
+            AdvanceTutorialStage();
+        }
+    }
+    private void AdvanceTutorialStage()
+    {
+        StartCoroutine(AdvanceTutorialStageCoroutine());
     }
 
     // Update is called once per frame
@@ -23,31 +74,40 @@ public class Catapult : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(Launch());
+            if (tutorialStage == TutorialStage.Fire) AdvanceTutorialStage();
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             StartCoroutine(Reel());
+            if (tutorialStage == TutorialStage.Reload) AdvanceTutorialStage();
         }
-        if (Input.GetKey(KeyCode.D)) {
-            if (transform.parent.rotation.y > -0.60f) {
-                transform.parent.Rotate(0, -1f, 0);
+        if (!isReloading)
+        {
+            if (Input.GetKey(KeyCode.D)) {
+                if (transform.parent.rotation.y > -0.60f) {
+                    transform.parent.Rotate(0, -1f, 0);
+                }
+                if (tutorialStage == TutorialStage.RotateLeft) AdvanceTutorialStage();
             }
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            if (transform.parent.rotation.y < 0.60f) {
-                transform.parent.Rotate(0, 1f, 0);
+            if (Input.GetKey(KeyCode.A)) {
+                if (transform.parent.rotation.y < 0.60f) {
+                    transform.parent.Rotate(0, 1f, 0);
+                }
+                if (tutorialStage == TutorialStage.RotateRight) AdvanceTutorialStage();
             }
-        }
-        if (Input.GetKey(KeyCode.W)) {
-            if (ballForce < (originalForce * 2)) {
-                ballForce += (originalForce / 100f);
-                slider.value = ballForce;
+            if (Input.GetKey(KeyCode.W)) {
+                if (ballForce < (originalForce * 2)) {
+                    ballForce += (originalForce / 100f);
+                    // slider.value = ballForce;
+                }
+                if (tutorialStage == TutorialStage.ForceIncrease) AdvanceTutorialStage();
             }
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            if (ballForce > (originalForce / 20)) {
-                ballForce -= (originalForce / 100f);
-                slider.value = ballForce;
+            if (Input.GetKey(KeyCode.S)) {
+                if (ballForce > (originalForce / 20)) {
+                    ballForce -= (originalForce / 100f);
+                    // slider.value = ballForce;
+                }
+                if (tutorialStage == TutorialStage.ForceDecrease) AdvanceTutorialStage();
             }
         }
     }
@@ -65,17 +125,22 @@ public class Catapult : MonoBehaviour
 
     IEnumerator Reel()
     {
-        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 10f);
-        yield return new WaitForSeconds(.5f);
-        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0f);
-        yield return new WaitForSeconds(1.5f);
-        setBall(Instantiate(myBall, transform.Find("Spawn").position, Quaternion.identity));
+        if (!isReloading)
+        {
+            isReloading = true;
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 10f);
+            yield return new WaitForSeconds(.5f);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0f);
+            yield return new WaitForSeconds(1.5f);
+            setBall(Instantiate(myBall, transform.Find("Spawn").position, Quaternion.identity));
+            isReloading = false;
+        }
     }
 
     private void setBall(GameObject newBall) {
         curBall = newBall;
         originalForce = curBall.GetComponent<ConstantForce>().force.y;
         ballForce = originalForce;
-        slider.value = ballForce;
+        //slider.value = ballForce;
     }
 }
