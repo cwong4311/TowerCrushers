160a659128aa1f5ad7a2d15583e767cb8de0e613
C. Wong <cwong4311@gmail.com>
2019-11-14 01:14:39 +1100

    Adding LAN support and additional changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Linq;
 
@@ -13,6 +14,10 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var input = transform.Find("InputField").gameObject.GetComponentInChildren<InputField>();
+        var se= new InputField.SubmitEvent();
+        se.AddListener(SetIPAddress);
+        input.onEndEdit = se;
     }
 
     // Update is called once per frame
@@ -29,6 +34,24 @@ public class MainMenu : MonoBehaviour
         gameObject.SetActive(false);
         Camera.main.enabled = false;
         gameState.GetComponent<Main>().mode = Modes.SINGLE;
+    }
+
+    public void ShowMultiplayer()
+    {
+        transform.Find("InputField").gameObject.SetActive(true);
+        transform.Find("HostGame").gameObject.SetActive(true);
+        transform.Find("JoinGame").gameObject.SetActive(true);
+
+        transform.Find("Practice").gameObject.SetActive(false);
+        transform.Find("Multiplayer").gameObject.SetActive(false);
+        transform.Find("Exit").gameObject.SetActive(false);
+    }
+
+    public void SetIPAddress(string arg0)
+    {
+        manager.networkAddress = arg0;
+
+        transform.Find("JoinGame").GetComponent<Button>().interactable = true;
     }
 
     public void HostGame()
